48730a30c846f1691bf1c8d89190eebab8d4b87e
sattarmohd <sattar78692md@gmail.com>
2019-10-21 11:25:40 +0530

    person detail

--- app/src/main/java/com/beingmomin/mominapp/data/DataManager.kt
+++ app/src/main/java/com/beingmomin/mominapp/data/DataManager.kt
@@ -11,23 +11,23 @@ import javax.inject.Inject
 /**
  * Created by sam on 19/3/18.
  */
-class DataManager @Inject constructor( private  val appPrefHelper: AppPreferencesHelper,
+class DataManager @Inject constructor(private val appPrefHelper: AppPreferencesHelper,
                                       private val apiHelper: AppApiHelper) : dtManager {
 
     /**
      * start of shared preferences functions
      */
-    override fun setAccessToken(accessToken: String)=appPrefHelper.setAccessToken(accessToken)
+    override fun setAccessToken(accessToken: String) = appPrefHelper.setAccessToken(accessToken)
 
-    override fun getAccessToken(): String =appPrefHelper.getAccessToken()
+    override fun getAccessToken(): String = appPrefHelper.getAccessToken()
 
-    override fun setUsername(username: String) =appPrefHelper.setUsername(username)
+    override fun setUsername(username: String) = appPrefHelper.setUsername(username)
 
-    override fun getUsername(): String=appPrefHelper.getUsername()
+    override fun getUsername(): String = appPrefHelper.getUsername()
 
-    override fun setIsLoginFlag(flag: Boolean)=appPrefHelper.setIsLoginFlag(flag)
+    override fun setIsLoginFlag(flag: Boolean) = appPrefHelper.setIsLoginFlag(flag)
 
-    override fun getIsLogin(): Boolean=appPrefHelper.getIsLogin()
+    override fun getIsLogin(): Boolean = appPrefHelper.getIsLogin()
 
     override fun setLocality(locality: String) = appPrefHelper.setLocality(locality)
 
@@ -37,9 +37,9 @@ class DataManager @Inject constructor( private  val appPrefHelper: AppPreference
 
     override fun setUserId(userid: Int) = appPrefHelper.setUserId(userid)
 
-    override fun getUserId(): Int =appPrefHelper.getUserId()
+    override fun getUserId(): Int = appPrefHelper.getUserId()
 
-    override fun setLocalityId(localityid: Int)= appPrefHelper.setLocalityId(localityid)
+    override fun setLocalityId(localityid: Int) = appPrefHelper.setLocalityId(localityid)
 
     override fun getLocalityId(): Int = appPrefHelper.getLocalityId()
 
@@ -51,15 +51,15 @@ class DataManager @Inject constructor( private  val appPrefHelper: AppPreference
      * start of api call functions
      */
 
-    override fun  doLoginApiCall(request: LoginApiBody)=apiHelper.doLoginApiCall(request)
+    override fun doLoginApiCall(request: LoginApiBody) = apiHelper.doLoginApiCall(request)
 
-    override fun doSignUpApiCall(request:  SignUpApiBody): Single<SignUpResponse> = apiHelper.doSignUpApiCall(request)
+    override fun doSignUpApiCall(request: SignUpApiBody): Single<SignUpResponse> = apiHelper.doSignUpApiCall(request)
 
     override fun doLocalityAmbassadorApiCall(): Single<LocalityAmbassadorsResponse> = apiHelper.doLocalityAmbassadorApiCall()
 
     override fun doSearchPersonApiCall(request: SearchPersonApiBody): Single<SearchPersonResponse> = apiHelper.doSearchPersonApiCall(request)
 
-    override fun doAddPersonApiCall(request: AddPersonApiBody, profileFile: File): Single<AddPersonResponse> = apiHelper.doAddPersonApiCall(request,profileFile)
+    override fun doAddPersonApiCall(request: AddPersonApiBody, profileFile: File): Single<AddPersonResponse> = apiHelper.doAddPersonApiCall(request, profileFile)
 
     override fun doGetLocalitiesApiCall(): Single<GetLocalitiesResponse> = apiHelper.doGetLocalitiesApiCall()
 
@@ -69,12 +69,11 @@ class DataManager @Inject constructor( private  val appPrefHelper: AppPreference
 
     override fun doGetDetailedPersonApiCall(request: GetDetailedPersonBody): Single<GetDetailedPersonResponse> = apiHelper.doGetDetailedPersonApiCall(request)
 
-    override fun doAddNewsApiCall(request: AddNewsApiBody, newsAttachment: File?): Single<AddNewsResponse> = apiHelper.doAddNewsApiCall(request,newsAttachment)
+    override fun doAddNewsApiCall(request: AddNewsApiBody, newsAttachment: File?): Single<AddNewsResponse> = apiHelper.doAddNewsApiCall(request, newsAttachment)
 
     /**
      * end of api call functions
      */
-
 
 
 }
